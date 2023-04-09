7791ff781032ebdd198c46ba28020e7426077f80
Miguel <191k0019@itscarrillopuerto.edu.mx>
2023-04-09 13:27:58 -0500

    cambios sw4

--- sw.js
+++ sw.js
@@ -14,14 +14,14 @@ const APP_SHELL = [
     'img/avatars/wolverine.jpg',
     'img/avatars/thor.jpg',
     'js/app.js',
-    'js/sw-utils.js'
+    'js/sw-utils.js',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
 ];
 
 // cosas que no van a cambiar
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     'css/animate.css',
     'js/libs/jquery.js'
 ];
